7a89af1de6c631724ccd06c61f8a7aa2fa697f79
Gerkul <90365532+Gerkul@users.noreply.github.com>
2023-02-25 10:17:38 +1000

    Added Arduino code for led, button and servo

---
+++ microcontroller/code/arduino/esp32button/esp32button.ino
@@ -0,0 +1,16 @@
+int ledPin = 14;
+int buttonPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(buttonPin, HIGH);
+  if (digitalRead(buttonPin)) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+}
